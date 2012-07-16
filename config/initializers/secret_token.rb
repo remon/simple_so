1c926648a56953bd219015605803e05c20f30826
Reymond <remmony@gmail.com>
2012-07-17 01:26:15 +0300

    Update master

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Auth2::Application.config.secret_token = '39baace845c3428c10b8d41c5db93be2a1767993941a3bd38ff60f9579666f8cf46e2b804125d5c0a110e055c09527895b0f3ce005f16604d21475246fb07fc5'
+SimpleSocial1::Application.config.secret_token = '39baace845c3428c10b8d41c5db93be2a1767993941a3bd38ff60f9579666f8cf46e2b804125d5c0a110e055c09527895b0f3ce005f16604d21475246fb07fc5'
